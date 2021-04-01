5bcf163cd54d913d769fe11675b7d3fe2ea7781c
jonassvalin <jonassvalin@gmail.com>
2021-04-01 10:48:53 +0200

    Add fetch-users-by-last-name capability

--- src/spike_crux/core.clj
+++ src/spike_crux/core.clj
@@ -2,13 +2,35 @@
   (:require [crux.api :as crux]
             [clojure.set :as set]))
 
+(defn- map-id [user]
+  (set/rename-keys user {:crux.db/id :id}))
+
+(defn- map-ids [users]
+  (map #(->
+          %
+          first
+          map-id)
+       users))
+
 (defn create-user [node user]
   (crux/submit-tx node [[:crux.tx/put (set/rename-keys user {:id :crux.db/id})]])
   user)
 
-(defn fetch-user [node id]
+(defn fetch-user-by-id [node id]
   (->
     node
     (crux/db)
     (crux/entity id)
     (set/rename-keys {:crux.db/id :id})))
+
+(defn fetch-users-by-last-name [node last-name]
+  (->
+    node
+    (crux/db)
+    (crux/q
+      {:find          '[element]
+       :where         '[[element :last-name n]]
+       :args          [{'n last-name}]
+       :full-results? true})
+    (map-ids)
+    ))
--- test/spike_crux/core_test.clj
+++ test/spike_crux/core_test.clj
@@ -7,22 +7,57 @@
 (deftest create-user-test
   (testing "returns created user"
     (let [node (crux/start-node {})
-          user {:id :123
+          user {:id         :123
                 :first-name "Bob"
                 :last-name  "Andersson"}
           created-user (create-user node user)]
       (is (= user created-user)))))
 
-(deftest fetch-user-test
+(deftest fetch-user-by-id-test
   (testing "persists user"
     (let [node (crux/start-node {})
           id :123
-          user {:id id
+          user {:id         id
                 :first-name "Bob"
                 :last-name  "Andersson"}
           equal-user? #(= user %)
           _ (create-user node user)]
       (fact
-        (fetch-user node id)
+        (fetch-user-by-id node id)
         =eventually=>
         equal-user?))))
+
+(deftest fetch-users-by-last-name-test
+  (testing "returns single user"
+    (let [node (crux/start-node {})
+          last-name "Andersson"
+          bob {:id         :123
+               :first-name "Bob"
+               :last-name  last-name}
+          ted {:id         :456
+               :first-name "Ted"
+               :last-name  "Tedsson"}
+          equal-bob? #(= [bob] %)]
+      (create-user node bob)
+      (create-user node ted)
+      (fact
+        (fetch-users-by-last-name node last-name)
+        =eventually=>
+        equal-bob?)))
+
+  (testing "returns multiple users"
+    (let [node (crux/start-node {})
+          last-name "Andersson"
+          bob {:id         :123
+               :first-name "Bob"
+               :last-name  last-name}
+          ted {:id         :456
+               :first-name "Ted"
+               :last-name  last-name}
+          equal-users? #(= [ted bob] %)]
+      (create-user node bob)
+      (create-user node ted)
+      (fact
+        (fetch-users-by-last-name node last-name)
+        =eventually=>
+        equal-users?))))
